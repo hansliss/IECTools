be72fd0cdbe605763f93a0319b5c7f6b4a91fa12
Hans Liss <Hans@Liss.nu>
2020-09-24 16:53:05 +0200

    Initial import

---
+++ getPerson.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from zeep import Client
+import uuid
+import sys
+import configparser
+
+## Read command-line parameters and configuration file
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+ID = sys.argv[2]
+wsdl = config['IEC']['wsdl']
+endpoint = config['IEC']['endpoint']
+sessiontoken = config['IEC']['sessiontoken']
+
+## Create a SOAP client and from that, create a new service with the correct endpoint
+client_tmp = Client(wsdl)
+service = client_tmp.create_service(client_tmp.service._binding.name, endpoint)
+
+## Request data should contain whatever is in the 'request' subdocument within the
+## SOAP request XML
+request_data={'request' : {'SessionToken' : uuid.UUID('{' + sessiontoken + '}'),
+                           'MessageId' : uuid.uuid4(),
+                           'Id' : ID}}
+
+## Call the method and get a response object
+response=service.GetCardholderById(**request_data)
+
+## Print out the result
+print("PNR: " + response.FreeInfo1)
+print("Name: " + response.FirstName + " " + response.Surname)
+
